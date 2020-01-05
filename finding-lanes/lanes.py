fa9e9b8c4e7e3ccb3b8e9746f7e5a81dc757368a
lopeselio <lopeselio@gmail.com>
2020-01-05 23:39:54 +0530

    converting image to grayscale

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -1,5 +1,8 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 cv2.imshow('result', image)
 cv2.waitKey(0)
