a6a42816f1d073fc3c9203d654473744eae59a97
lopeselio <lopeselio@gmail.com>
2020-01-09 23:50:26 +0530

    Grayscale image was smoothened and Edge Detection is applied

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -4,5 +4,7 @@ import numpy as np
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow('result', image)
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+canny = cv2.Canny(blur, 50, 150)
+cv2.imshow('result', canny)
 cv2.waitKey(0)
