c13833a841f743cbcc4275d21ebf44cfbaddb47c
lopeselio <lopeselio@gmail.com>
2020-01-11 14:26:04 +0530

    fetched the region of interest

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -1,10 +1,26 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+ 
+def canny(image):
+  gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+  blur = cv2.GaussianBlur(gray, (5,5), 0)
+  canny = cv2.Canny(blur, 50, 150)
+  return canny
 
-image = cv2.imread('test_image.jpg')
+def region_of_interest(image):
+  height = image.shape[0]
+  polygons =  np.array([[(200, height), (1100, height), (550, 250)]])
+  mask = np.zeros_like(image)
+  cv2.fillPoly(mask, polygons, 255)
+  return mask
+
+image = cv2.imread('./../Image/test_image.jpg')
 lane_image = np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-canny = cv2.Canny(blur, 50, 150)
-cv2.imshow('result', canny)
+canny = canny(lane_image)
+
+
+cv2.imshow("result", region_of_interest(canny))
 cv2.waitKey(0)
+
+
